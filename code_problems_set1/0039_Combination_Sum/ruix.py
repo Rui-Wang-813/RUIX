6e3f53ed0e3e1e43a2807db70e526782102d1aec
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-31 21:05:11 -0800

    [0039][ver 1.1] Combination Sum --fix some bugs

--- code_problems_set1/0039_Combination_Sum/ruix.py
+++ code_problems_set1/0039_Combination_Sum/ruix.py
@@ -6,14 +6,14 @@ class Solution:
         candidates.sort()   # sort the candidates
 
         def search(cand: List[int], stk: List[int], target: int) -> None:
-            i, n = 0, len(candidates)
+            i, n = 0, len(cand)
             while i < n:
                 if cand[i] > target:
                     # as the cand is sorted, no further elements will be included.
                     return 
                 if cand[i] == target:
                     # as the cand is sorted, only this element will be included
-                    res.append(stk + [candidates[i]])
+                    res.append(stk + [cand[i]])
                     return 
                 else:
                     # this handles the case where cand[i] is included.
