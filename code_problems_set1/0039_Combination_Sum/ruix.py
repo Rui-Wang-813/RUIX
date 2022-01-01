5b1c805b3354edf89a1c16f269ecbc980584ff96
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-31 20:53:40 -0800

    [0039][ver 1.0] Combination Sum

---
+++ code_problems_set1/0039_Combination_Sum/ruix.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()   # sort the candidates
+
+        def search(cand: List[int], stk: List[int], target: int) -> None:
+            i, n = 0, len(candidates)
+            while i < n:
+                if cand[i] > target:
+                    # as the cand is sorted, no further elements will be included.
+                    return 
+                if cand[i] == target:
+                    # as the cand is sorted, only this element will be included
+                    res.append(stk + [candidates[i]])
+                    return 
+                else:
+                    # this handles the case where cand[i] is included.
+                    search(cand[i:], stk + [cand[i]], target-cand[i])
+                
+                i += 1
+
+        search(candidates, [], target)
+        
+        return res
